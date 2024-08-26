495a056f5c99b4c7dfea75c6f643eb18f6b1ad7f
Yami939 <costaferafael678@gmail.com>
2024-08-26 12:30:46 -0300

    commit

--- api.py
+++ api.py
@@ -1,13 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 movies = [
-    {'id':1, 'titulo':'Star Wars', 'genero':'sci-fi'},
-    {'id':2, 'titulo':'It', 'genero':'Horror'},
-    {'id':3, 'titulo':'Um tira da pesada', 'genero':'commedy'}
+    {'id':1, 'title':'Star Wars', 'genre':'sci-fi'},
+    {'id':2, 'title':'It', 'genre':'Horror'},
+    {'id':3, 'title':'Um tira da pesada', 'gen':'commedy'}
 ]
 
-@app.route('/api/movies')
+@app.route('/api/movies', methods =['GET'])
 def get_movies():
     return jsonify(movies)
+
+@app.route('/api/movies/<int:id>', methods =['GET'])
+def get_movie(id):
+    movie = next(
+        (movie for movie in movies if movie ['id'] == id),
+        None
+    )
+    return jsonify(movie) if movie else ('', 404)
+
+@app.route('/api/movies', methods = ['POST'])
+def add_movie():
+    new_movie = request.get_json()
+    movies.append(new_movie)
+    return jsonify(new_movie), 201
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
